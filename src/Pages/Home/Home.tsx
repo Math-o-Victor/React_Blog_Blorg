968eaddd8bc056052b009554a5182a344f15f3d9
Math-o-Victor <106315792+Math-o-Victor@users.noreply.github.com>
2022-07-27 10:47:37 -0300

    Commit Inicial

---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+  return (
+    <>
+      <h1 className='titulo'>Welcome home!</h1>
+      <img src="https://i.gifer.com/1ynM.gif" alt="Loop de cores" />
+    </>
+  )
+}
+
+export default Home
