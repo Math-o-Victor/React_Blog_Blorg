228c864f2ff63665c4757c50a8053de354e0a777
Math-o-Victor <106315792+Math-o-Victor@users.noreply.github.com>
2022-08-16 12:28:54 -0300

    Apagando tudo só pra ter certeza

--- src/models/UsuarioLogin.ts
+++
@@ -1,9 +0,0 @@
-interface UsuarioLogin{
-
-    id: number
-    usuario: string
-    senha: string
-    token?: string|null
-}
-
-export default UsuarioLogin;
--- src/store/Store.ts
+++
@@ -1,6 +0,0 @@
-import { createStore } from "redux";
-import { tokenReducer } from "./Tokens/tokensReducer";
-
-const Store = createStore(tokenReducer)
-
-export default Store;
